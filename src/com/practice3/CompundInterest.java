6f00222c25181eca9952a0fb06e8fa92a5aecf75
Prakhyath Shri <prakhyath2004@gmail.com>
2025-08-30 19:08:35 +0530

    Compound Interest

---
+++ src/com/practice3/CompundInterest.java
@@ -0,0 +1,41 @@
+package com.practice3;
+
+import java.util.Scanner;
+
+public class CompundInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter principal amount: ");
+        double p = sc.nextDouble();
+        System.out.print("Enter rate of interest: ");
+        double r = sc.nextDouble();
+        System.out.print("Enter time in years: ");
+        int t = sc.nextInt();
+        System.out.println("Enter the compounding rate per year ");
+        System.out.println("For Annually: Enter 1 ");
+        System.out.println("For Semi - Annually: Enter 2 ");
+        System.out.println("For Quarterly : Enter 3 ");
+        System.out.println("For Monthly: Enter 4 ");
+        System.out.println("For Daily: Enter 5 ");
+        int c = sc.nextInt();
+        int n = switch (c) {
+            case 1 -> 1;
+            case 2 -> 2;
+            case 3 -> 4;
+            case 4 -> 12;
+            case 5 -> 365;
+            default -> 1;
+        };
+        double result = 1;
+        double base = (1 + (r / 100) / n );
+        int exp = n * t;
+        for(int i = 1; i <= exp; i++){
+            result = base * result;
+        }
+        double a = p * result;
+        System.out.printf("Amount after Interest = %.2f\n", a);
+        double ci = a - p;
+        System.out.printf("Compound Interest = %.2f" , ci);
+    }
+}
