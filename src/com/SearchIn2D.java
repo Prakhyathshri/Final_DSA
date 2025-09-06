c360ce4543664c2c6945048ef29739dc1be83f6c
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-06 11:55:05 +0530

    Perfect Number

---
+++ src/com/SearchIn2D.java
@@ -0,0 +1,11 @@
+//package com;
+//
+//public class SearchIn2D {
+//    public static void main(String[] args) {
+//        int[][] arr = {
+//            {23, 4, 1},
+//            {18, 12, 3, 9},
+//                {78,99,34,56}
+//
+//    };
+//}
--- src/com/practice4/PerfectNumber.java
+++ src/com/practice4/PerfectNumber.java
@@ -8,6 +8,31 @@ public class PerfectNumber {
 
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
+        int sum = 0;
+
+        if (num < 0){
+            System.out.println("Invalid input");
+            return;
+        }
+
+        if (num == 0){
+            System.out.println("0 is not considered as perfect number");
+            return;
+        }
+
+        for (int i = 1; i <= num/2; i++){
+            if (num % i == 0){
+                sum = sum + i;
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("Sum of divisors: " + sum);
+        if(sum == num){
+            System.out.println("It is a perfect number");
+        } else {
+            System.out.println("It is a NOT a perfect number");
+        }
 
 
     }
