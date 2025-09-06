a01169156d2177f044f52131db3b591ccf605c04
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-06 14:27:28 +0530

    Vowel and Consonant

---
+++ src/com/practice4/FutureValueInvestment.java
@@ -0,0 +1,22 @@
+//package com.practice4;
+//
+//import java.util.Scanner;
+//
+//public class FutureValueInvestment {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter investment amount: ");
+//        double amount = sc.nextDouble();
+//
+//        System.out.println("Enter annual interest rate (in %): ");
+//        double rate = sc.nextDouble();
+//
+//        System.out.println("Enter number of years: ");
+//        int years = sc.nextInt();
+//
+//        double temp;
+//        for(int i = 1; i <= )
+//        double futureValue = amount * (1 + rate)
+//    }
+//}
