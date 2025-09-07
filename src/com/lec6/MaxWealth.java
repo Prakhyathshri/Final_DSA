830d52b967db6613cf81b799d8e653b1512a3efa
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-07 22:10:28 +0530

    Max Wealth

---
+++ src/com/lec6/MaxWealth.java
@@ -0,0 +1,30 @@
+package com.lec6;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+
+        int[][] accounts = {{1,5},
+                            {7,3,},
+                            {3,5}
+        };
+
+        System.out.println(maximumWealth(accounts));
+    }
+
+
+    //Can use enhanced for loop when you are not using the index anywhere
+    //Using the i and j directly
+    static int maximumWealth(int[][] accounts) {
+        int x = Integer.MIN_VALUE;
+        for(int i = 0; i < accounts.length; i++){
+            int sum = 0;
+            for(int j = 0; j < accounts[i].length; j++){
+                sum = sum + accounts[i][j];
+            }
+            if (sum > x){
+                x = sum;
+            }
+        }
+        return x;
+    }
+}
