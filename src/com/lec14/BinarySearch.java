380c16597b05c5ab61d95a00e36866c77c67a898
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-08 15:23:58 +0530

    Two numbers

---
+++ src/com/lec14/BinarySearch.java
@@ -0,0 +1,9 @@
+//package com.lec14;
+//
+//import java.util.Arrays;
+//
+//public class BinarySearch {
+//    public static void main(String[] args) {
+//
+//    }
+//}
--- src/com/lec14/Main.java
+++
@@ -1,4 +0,0 @@
-package com.lec14;
-
-public class Main {
-}
