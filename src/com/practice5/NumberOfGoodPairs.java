bb6adf5c5ab6e6626d665a578365814087c33b41
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-14 19:28:06 +0530

    Number of good pairs

---
+++ src/com/practice5/NumberOfGoodPairs.java
@@ -0,0 +1,22 @@
+package com.practice5;
+//https://leetcode.com/problems/number-of-good-pairs/description/
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1};
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    static int numIdenticalPairs(int[] nums) {
+        int pair = 0;
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums.length; j++){
+                if (i < j){
+                    if(nums[i] == nums[j]){
+                        pair++;
+                    }
+                }
+            }
+        }
+        return pair;
+    }
+}
