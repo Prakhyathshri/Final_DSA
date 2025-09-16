388482bec5413aa2298f03f4383a2ca10cf18501
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-16 10:20:24 +0530

    Count Items Matching a Rule

--- src/com/practice5/CountItemsMatchRule.java
+++ src/com/practice5/CountItemsMatchRule.java
@@ -1,16 +1,38 @@
-//package com.practice5;
-//
-//public class CountItemsMatchRule {
-//    public static void main(String[] args) {
+package com.practice5;
+
+//https://leetcode.com/problems/count-items-matching-a-rule/description/
+public class CountItemsMatchRule {
+    public static void main(String[] args) {
 //        String[][] items = {{"phone", "blue", "pixel"},
-//                            {"computer", "silver", "lenovo"},
-//                            {"phone", "gold", "iphone"}
+//                {"computer", "silver", "lenovo"},
+//                {"phone", "gold", "iphone"}
 //        };
 //        String ruleKey = "color", ruleValue = "silver";
-//        System.out.println(countMatches(items, ruleKey, ruleValue));
-//
-//    }
-//    static int countMatches(String[][] items, String ruleKey, String ruleValue) {
-//
-//    }
-//}
+
+        String[][] items = {{"phone","blue","pixel"},
+                {"computer","silver","phone"},
+                {"phone","gold","iphone"}
+        };
+        String ruleKey = "type", ruleValue = "phone";
+        System.out.println(countMatches(items, ruleKey, ruleValue));
+
+    }
+
+    static int countMatches(String[][] items, String ruleKey, String ruleValue) {
+        int temp = 0;
+        if (ruleKey.equals("type")) {
+            temp = 0;
+        } else if (ruleKey.equals("color")) {
+            temp = 1;
+        } else {
+            temp = 2;
+        }
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+                if (ruleValue.equals(items[i][temp])) {
+                    count++;
+                }
+        }
+        return count;
+    }
+}
--- src/com/practice5/TargetArrayInOrder.java
+++ src/com/practice5/TargetArrayInOrder.java
@@ -1,4 +1,4 @@
-package com;
+package com.practice5;
 
 import java.util.ArrayList;
 import java.util.Arrays;
