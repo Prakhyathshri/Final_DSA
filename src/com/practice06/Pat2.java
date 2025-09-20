15b1436118c9c6c08320d349d18b7f9bde42e534
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-20 11:26:40 +0530

    Pattern 2

--- src/com/practice06/Pat2.java
+++ src/com/practice06/Pat2.java
@@ -1,4 +1,18 @@
 package com.practice06;
 
+import java.util.Scanner;
+
 public class Pat2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the ending digit: ");
+        int end = sc.nextInt();
+
+        for (int i = 1; i <= end; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
