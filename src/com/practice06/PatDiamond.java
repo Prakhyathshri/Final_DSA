5097403430f9b64095d2f313b90e7ee21f1d05d6
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-20 14:47:37 +0530

    Pattern Diamond

---
+++ src/com/practice06/PatDiamond.java
@@ -0,0 +1,26 @@
+package com.practice06;
+
+public class PatDiamond {
+    public static void main(String[] args) {
+
+        int row = 3;
+        for(int i = 1; i <= row; i++){
+            for(int j = 1; j <= row - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i * 2 - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = row - 1; i >= 1; i--){
+            for(int j = 1; j <= row - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i * 2 - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
