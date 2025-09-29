de5c0ddb996caca4c129719c693942c66861f1a6
Prakhyath Shri <prakhyath2004@gmail.com>
2025-09-29 15:20:27 +0530

    Plus one

--- src/com/practice05/ArrayFormInt.java
+++ src/com/practice05/ArrayFormInt.java
@@ -11,44 +11,16 @@ public class ArrayFormInt {
     public static void main(String[] args) {
         int[] num1 = {1, 2, 0, 0};
         int k1 = 34;
-
-//        int[] num2 = {2, 7, 4};
-//        int k2 = 181;
-//
-//        int[] num3 = {2, 1, 5};
-//        int k3 = 806;
-
-        System.out.println(addToArrayForm(num1, k1));
-//        System.out.println(addToArrayForm(num2, k2));
-//        System.out.println(addToArrayForm(num3, k3));
-
+        addToArrayForm(num1);
+        System.out.println(num1);
     }
 
-    static List<Integer> addToArrayForm(int[] num, int k) {
-        System.out.println(Arrays.toString(num));
-        int n = 0;
-        for (int i = 0; i <= num.length - 1; i++) {
-            n = n * 10 + num[i];
+    static void addToArrayForm(int[] num) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = num.length - 1; i <= 0; i--){
+            list.add(num[i]);
         }
-        n = n + k;
-        System.out.println(n);
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < num.length; i++) {
-            list.add(0, n % 10);
-            n /= 10;
-        }
-        return list;
-    }
 
-    /*
-    while (i >= 0 || k > 0) {
-    if (i >= 0) {
-        k += num[i];  // add current digit + carry
-        i--;
+        System.out.println(list);
     }
-    result.add(k % 10);  // store last digit
-    k /= 10;             // carry for next step
-}
-
-     */
 }
--- src/com/practice05/PlusOne.java
+++ src/com/practice05/PlusOne.java
@@ -1,7 +1,28 @@
 package com.practice05;
 
+import java.util.Arrays;
+
 public class PlusOne {
     public static void main(String[] args) {
+        int[] digits = {1,2,9};
+        int[] digits2 = {1, 2, 9};
+        int[] digits3 = {9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+        System.out.println(Arrays.toString(plusOne(digits2)));
+        System.out.println(Arrays.toString(plusOne(digits3)));
+    }
 
+    static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] output = new int[digits.length + 1];
+        output[0] = 1;
+        return output;
     }
 }
