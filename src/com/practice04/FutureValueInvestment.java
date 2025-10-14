e95528b838c376ffd1a6d80d18fd9c3fbeb8f981
Prakhyath Shri <prakhyath2004@gmail.com>
2025-10-14 15:54:10 +0530

    Future investment value

--- src/com/practice04/FutureValueInvestment.java
+++ src/com/practice04/FutureValueInvestment.java
@@ -1,22 +1,34 @@
-//package com.practice4;
-//
-//import java.util.Scanner;
-//
-//public class FutureValueInvestment {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("Enter investment amount: ");
-//        double amount = sc.nextDouble();
-//
-//        System.out.println("Enter annual interest rate (in %): ");
-//        double rate = sc.nextDouble();
-//
-//        System.out.println("Enter number of years: ");
-//        int years = sc.nextInt();
-//
-//        double temp;
-//        for(int i = 1; i <= )
-//        double futureValue = amount * (1 + rate)
-//    }
-//}
+package com.practice04;
+
+import java.util.Scanner;
+
+public class FutureValueInvestment {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter investment amount: ");
+        double p = sc.nextDouble();
+
+        System.out.print("Enter annual interest rate (in %): ");
+        double r = sc.nextDouble();
+
+        System.out.print("Enter number of years: ");
+        int t = sc.nextInt();
+
+        System.out.print("Enter number of times interest is compounded per year: ");
+        int n = sc.nextInt();
+
+        r = r / 100;
+
+        double ans = p * power((1 + r/n), n * t);
+        System.out.printf("The future value is: %.2f", ans);
+    }
+
+    static double power(double base, int pow){
+        double ans = 1;
+        for (int i = 1; i <= pow; i++){
+            ans = ans * base;
+        }
+        return ans;
+    }
+}
