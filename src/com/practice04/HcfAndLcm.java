539cd9f33fdf6be1848854761a2ca7850cdf13bd
Prakhyath Shri <prakhyath2004@gmail.com>
2025-10-14 11:11:09 +0530

    Euclids HCF and LCM

--- src/com/practice04/HcfAndLcm.java
+++ src/com/practice04/HcfAndLcm.java
@@ -2,22 +2,32 @@ package com.practice04;
 
 public class HcfAndLcm {
     public static void main(String[] args) {
-        int a = 12;
-        int b = 18;
+        int a = 18;
+        int b = 48;
         System.out.printf("HCF of %d and %d is: %d", a, b, hcf(a, b));
         System.out.println();
         System.out.printf("LCM of %d and %d is: %d", a, b, lcm(a, b));
         System.out.println();
     }
 
-    static int hcf(int a, int b){
-        int hcfans = 1;
-        for (int i = 1; i <= a && i <= b; i++){
-            if (a % i == 0 && b % i == 0){
-                hcfans = i;
-            }
+//    static int hcf(int a, int b){
+//        int hcfans = 1;
+//        for (int i = 1; i <= a && i <= b; i++){
+//            if (a % i == 0 && b % i == 0){s
+//                hcfans = i;
+//            }
+//        }
+//        return hcfans;
+//    }
+
+// EUCLID's algorithm - most efficient way to solve HCF and LCM
+    static int hcf(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        return hcfans;
+        return a;
     }
 
     static int lcm(int a, int b){
