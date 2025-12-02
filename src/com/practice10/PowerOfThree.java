d53fbef83e9ed7c628e384ca43beb16d11ca8077
Prakhyath Shri <prakhyath2004@gmail.com>
2025-12-02 08:10:55 +0530

    Power of Three

---
+++ src/com/practice10/PowerOfThree.java
@@ -0,0 +1,20 @@
+package com.practice10;
+
+public class PowerOfThree {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(3));
+    }
+
+    static boolean isPowerOfThree(int n) {
+        if (n <= 0){
+            return false;
+        }
+        while (n > 1){
+            if (n % 3 != 0){
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
