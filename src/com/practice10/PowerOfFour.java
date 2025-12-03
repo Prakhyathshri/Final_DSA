3de97b3fe83d747eb5a277c28045c680c5eb49cc
Prakhyath Shri <prakhyath2004@gmail.com>
2025-12-03 23:49:07 +0530

    Power of four

---
+++ src/com/practice10/PowerOfFour.java
@@ -0,0 +1,20 @@
+package com.practice10;
+
+public class PowerOfFour {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(20));
+    }
+
+    static boolean isPowerOfFour(int n){
+        if (n <= 0){
+            return false;
+        }
+        while (n > 1){
+            if (n % 4 != 0){
+                return false;
+            }
+            n /= 4;
+        }
+        return true;
+    }
+}
